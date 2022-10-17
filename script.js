8a0e3ffb527f22a15fb619d6f4021839a0bbdcc5
AndreFelipe94 <78113291+AndreFelipe94@users.noreply.github.com>
2022-10-17 17:03:27 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+console.log(document.querySelector('.menu'))
+
+
+window.onload = (event) => {
+const menu = document.querySelector('.menu');
+const NavMenu = document.querySelector('.nav-menu');
+
+
+menu.addEventListener('click', () => {
+    menu.classList.toggle('ativo');
+    NavMenu.classList.toggle('ativo')
+})
+}
